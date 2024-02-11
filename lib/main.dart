5578804fa013eebcadf5823b575a6bbe72d8ef03
tk <tykeaboyloy@gmail.com>
2024-02-12 00:17:35 +0700

    added favorites list page

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-  var favorite = <WordPair>[];
+  var favorites = <WordPair>[];
 
   void getNext() {
     current = WordPair.random();
@@ -40,64 +40,133 @@ class MyAppState extends ChangeNotifier {
   }
 
   void toggleWordPairs() {
-    if (favorite.contains(current)) {
-      favorite.remove(current);
+    if (favorites.contains(current)) {
+      favorites.remove(current);
     } else {
-      favorite.add(current);
+      favorites.add(current);
     }
+    notifyListeners();
+  }
+
+  void removeFavorite(word) {
+    favorites.remove(word);
     notifyListeners();
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme.titleLarge?.copyWith(
+        color: theme.colorScheme.onPrimary, fontWeight: FontWeight.bold);
+    var appState = context.watch<MyAppState>();
+    var pairList = appState.favorites;
+
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = const GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage(
+          paireList: pairList,
+        );
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: theme.colorScheme.background,
+            title: Row(
+              children: [
+                Text("Ressenti", style: textTheme),
+              ],
+            ),
+          ),
+          body: Row(children: [
+            Expanded(
+              child: Container(
+                child: page,
+              ),
+            )
+          ]),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: 'Favorite',
+              ),
+            ],
+            currentIndex: selectedIndex,
+            onTap: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            backgroundColor: theme.colorScheme.background,
+            selectedItemColor: theme.colorScheme.primary,
+            unselectedItemColor: Colors.grey,
+          ));
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  const GeneratorPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme.titleLarge?.copyWith(
-        color: theme.colorScheme.onPrimary, fontWeight: FontWeight.bold);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Text("Ressenti", style: textTheme),
-          ],
-        ),
-        backgroundColor: theme.colorScheme.background,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleWordPairs();
-                  },
-                  icon: const Icon(Icons.favorite),
-                  label: const Text("Favorite"),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: const Text("Next"),
-                ),
-              ],
-            ),
-            Text(appState.favorite.map((e) => e.asLowerCase).join(", "))
-          ],
-        ),
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleWordPairs();
+                },
+                icon: Icon(icon),
+                label: const Text("Favorite"),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: const Text("Next"),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -125,6 +194,68 @@ class BigCard extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Text(pair.asLowerCase, style: style),
       ),
+    );
+  }
+}
+
+class SmallCard extends StatelessWidget {
+  const SmallCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    final theme = Theme.of(context);
+
+    final style = theme.textTheme.bodyLarge?.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  appState.removeFavorite(pair);
+                },
+                icon: const Icon(Icons.delete),
+              ),
+              Text(
+                pair.asLowerCase,
+                style: style,
+              )
+            ],
+          )),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({
+    super.key,
+    required this.paireList,
+  });
+
+  final List<WordPair> paireList;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${paireList.length} favorites wordpairs'),
+        ),
+        for (var pair in paireList) SmallCard(pair: pair)
+      ],
     );
   }
 }
