c7eb446a9e49a83a54a9a1499b03421c262b3ed5
tk <tykeaboyloy@gmail.com>
2024-02-14 22:50:48 +0700

    added ios homescreen widget

---
+++ ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  NewsWidgetsBundle.swift
+//  NewsWidgets
+//
+//  Created by TYKEABOYLOY on 14/2/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+        NewsWidgetsLiveActivity()
+    }
+}
